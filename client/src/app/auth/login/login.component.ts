b997fbbcf682bdf937bb7620bf3a0c59dee8d22f
Pavel <ambassadherdev2@anuitex.com>
2019-07-26 14:24:34 +0300

    Completed login component.

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -12,15 +12,32 @@ import {
     styleUrls: ['./login.component.sass']
 })
 export class LoginComponent {
+    // tslint:disable-next-line:max-line-length
+    public usernamePattern: RegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     public username: string;
+    public passwordPattern: RegExp = new RegExp(/[A-Za-z0-9_ ]{5,16}/);
     public password: string;
-    public info: string;
+
+    public isShowMessage: boolean;
+    public isErrorMessage: boolean;
+    public message: string;
+
+    private invalidCredentialsMessage = 'Invalid credentials.';
+    private unhandledErrorMessage = 'Something went wrong.';
+    private credentialRequirmentsMessage = 'Credentials do not meet requirments.';
 
     constructor(
         private authService: AuthService,
     ) { }
 
     public async login() {
+        const isCredentialsValid: boolean = this.testCredentials();
+        if (!isCredentialsValid) {
+            this.showErrorMessage(0, this.credentialRequirmentsMessage);
+
+            return;
+        }
+
         const requestModel: LoginRequestModel = {};
         requestModel.username = this.username;
         requestModel.password = this.password;
@@ -28,9 +45,38 @@ export class LoginComponent {
         const responseModel: LoginResponseModel = await this.authService.getToken(requestModel);
 
         if (responseModel.statusCode === 200) {
-            this.info = responseModel.token;
-        } else {
-            this.info = responseModel.reason;
+            this.isShowMessage = true;
+            this.isErrorMessage = false;
+            this.message = responseModel.token;
+        }
+        if (responseModel.statusCode !== 200) {
+            this.showErrorMessage(responseModel.statusCode);
+        }
+    }
+
+    public testCredentials(): boolean {
+        const result = this.usernamePattern.test(this.username) && this.passwordPattern.test(this.password);
+
+        return result;
+    }
+
+    public showErrorMessage(statusCode?: number, message?: string) {
+        if (message) {
+            this.isShowMessage = true;
+            this.isErrorMessage = true;
+            this.message = message;
+        }
+        if (statusCode && statusCode === 401) {
+            this.isShowMessage = true;
+            this.isErrorMessage = true;
+            this.message = this.invalidCredentialsMessage;
+            console.log(2);
+        }
+        if (statusCode && statusCode !== 401) {
+            this.isShowMessage = true;
+            this.isErrorMessage = true;
+            this.message = this.unhandledErrorMessage;
+            console.log(3);
         }
     }
 }
