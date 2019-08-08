7d8bc96c1821839510afad8a81740476779d312d
Pavel <ambassadherdev2@anuitex.com>
2019-08-08 19:07:13 +0300

    Added admin page.

--- client/src/app/library/edit-book/edit-book.component.ts
+++ client/src/app/library/edit-book/edit-book.component.ts
@@ -1,15 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnDestroy,
+} from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { BookService, CartService, AuthService } from 'src/app/services';
+
+import { BookModel, BookFilterModel } from 'src/app/shared/models';
 
 @Component({
-  selector: 'app-edit-book',
-  templateUrl: './edit-book.component.html',
-  styleUrls: ['./edit-book.component.sass']
+    selector: 'app-edit-book',
+    templateUrl: './edit-book.component.html',
+    styleUrls: ['./edit-book.component.scss']
 })
-export class EditBookComponent implements OnInit {
+export class EditBookComponent implements OnDestroy {
+    public allBooks: BookModel[] = new Array<BookModel>();
+    public paginatedBooks: BookModel[] = new Array<BookModel>();
+    public booksInCartIdList: string[] = new Array<string>();
+    public isAuthenticated = false;
 
-  constructor() { }
+    public nameFilter: string;
+    public priceFromFilter: number;
+    public priceToFilter: number;
+    public typeFilter: string;
 
-  ngOnInit() {
-  }
+    public page = 0;
+    private limit = 5;
 
+    private cartAddBookSubscription: Subscription;
+    private cartRemoveBookSubscription: Subscription;
+
+    constructor(
+        private authService: AuthService,
+        private bookService: BookService,
+        private cartService: CartService,
+    ) {
+        this.initialize();
+    }
+
+    ngOnDestroy(): void {
+        this.cartAddBookSubscription.unsubscribe();
+        this.cartRemoveBookSubscription.unsubscribe();
+    }
+
+    public addBookToCart(bookModel: BookModel): void {
+        this.cartService.addBookToCart(bookModel);
+    }
+
+    public async removeBookFromCart(bookModel: BookModel): Promise<void> {
+        await this.cartService.removeBookFromCart(bookModel);
+    }
+
+    public isBookInCart(bookModel: BookModel): boolean {
+        const isBookInCart: boolean = this.booksInCartIdList.includes(bookModel.id);
+
+        return isBookInCart;
+    }
+
+    public async filterOptionUpdated(): Promise<void> {
+        const bookFilterModel: BookFilterModel = {};
+
+        if (this.nameFilter && this.nameFilter.trim()) {
+            bookFilterModel.name = this.nameFilter;
+        }
+        if (this.priceFromFilter > 0) {
+            bookFilterModel.priceFrom = this.priceFromFilter;
+        }
+        if (this.priceToFilter > 0) {
+            bookFilterModel.priceTo = this.priceToFilter;
+        }
+        if (this.typeFilter) {
+            bookFilterModel.type = this.typeFilter;
+        }
+
+        this.allBooks = await this.bookService.getFilteredBooks(bookFilterModel);
+
+        await this.clearPagingOptions();
+    }
+
+    public async clearPagingOptions(): Promise<void> {
+        this.page = 0;
+
+        await this.getPaginatedBooks(this.page);
+    }
+
+    public async getPaginatedBooks(page: number): Promise<void> {
+        const startIndex: number = page * this.limit;
+        const endIndex: number = startIndex + this.limit;
+        const paginatedBooks: BookModel[] = this.allBooks.slice(startIndex, endIndex);
+
+        if (this.allBooks && this.allBooks.length === 0) {
+            this.paginatedBooks = this.allBooks;
+            return;
+        }
+
+        if (paginatedBooks && paginatedBooks.length > 0) {
+            this.page = page;
+            this.paginatedBooks = paginatedBooks;
+        }
+    }
+
+    public hasContent(): boolean {
+        const hasBookContent: boolean = (this.allBooks && this.allBooks.length > 0);
+
+        return hasBookContent;
+    }
+
+    private async initialize(): Promise<void> {
+        this.allBooks = await this.bookService.getBooks();
+        await this.getPaginatedBooks(this.page);
+        this.booksInCartIdList = await this.cartService.getBookIdListFromCart();
+
+        this.cartAddBookSubscription = this.cartService.cartAddSubject.subscribe(book => {
+            if (book) {
+                this.booksInCartIdList.push(book.id);
+            }
+        });
+        this.cartRemoveBookSubscription = this.cartService.cartRemoveSubject.subscribe(book => {
+            if (book) {
+                this.booksInCartIdList = this.booksInCartIdList.filter(bookId => bookId !== book.id);
+            }
+        });
+
+        this.isAuthenticated = this.authService.isUserAuthenticated();
+    }
 }
