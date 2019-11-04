148d56c5756a8b8cc41a21716ed138df6f79a0f8
Dmytro Morhun <dimamorgun97@gmail.com>
2019-11-04 10:18:54 +0200

    client-react project was cleared from files which were not used for it.

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,68 +1,9 @@
 import React, { Component } from "react";
 
-import { Task } from "./models/task";
-import { NewTaskForm } from "./components/new-task-form";
-import { TasksList } from "./components/task-list";
-
-interface IState {
-  newTask: Task;
-  tasks: Task[];
-}
-
-class App extends Component<{}, IState> {
-  state = {
-    newTask: {
-      id: "1",
-      name: ""
-    },
-    tasks: []
-  };
-
-  private addTask = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    this.setState(previousState => ({
-      newTask: {
-        id: previousState.newTask.id + 1,
-        name: ""
-      },
-      tasks: [...previousState.tasks, previousState.newTask]
-    }));
-  }
-
-  private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      newTask: {
-        ...this.state.newTask,
-        name: event.target.value
-      }
-    });
-  }
-
-  private deleteTask = (taskToDelete: Task) => {
-    this.setState(previousState => ({
-      tasks: [
-        ...previousState.tasks.filter(task => task.id !== taskToDelete.id)
-      ]
-    }));
-  }
-
-  render() {
+class App extends Component {
+  render(): any {
     return (
-      <div>
-        <h2>Hello React TS!</h2>
-        <NewTaskForm
-          task={this.state.newTask}
-          onAdd={this.addTask}
-          onChange={this.handleTaskChange}
-        />
-        <div>
-          <TasksList
-            tasks={this.state.tasks}
-            onDelete={this.deleteTask}
-          />
-        </div>
-      </div>
+      <p>Comming soon!</p>
     );
   }
 }
--- client-react/src/models/task.ts
+++
@@ -1,5 +0,0 @@
-// tslint:disable-next-line: interface-name
-export interface Task {
-    id: string;
-    name: string;
-}
