32ff46b52eb1e065a10cbe0d3374106ae48f740e
Dmytro Morhun <dimamorgun97@gmail.com>
2019-11-05 10:36:06 +0200

    Simple `SignIn` flow was completed. Code for this example was written without Redux.

--- client-react/src/components/auth/SignIn.tsx
+++ client-react/src/components/auth/SignIn.tsx
@@ -1,10 +1,23 @@
-import React, { Component, FormEvent } from "react";
+import React, { Component, FormEvent, ReactElement } from "react";
+
+const API_ENDPOINT: string = "https://localhost";
+const AUTH_CONTROLLER_PATH: string = "auth";
+const LOGIN_ACTION_PATH: string = "login";
+const LOGGED_IN_USER_INFORMATION_ACTION_PATH: string = "me";
+const TEST_ADMIN_ACTION_PATH: string = "admin/test";
+
+interface ISignInModel {
+    username?: string;
+    password?: string;
+}
 
 export class SignIn extends Component {
     state = {
-        username: '',
-        password: '',
-    }
+        username: "",
+        password: "",
+        token: "",
+        textareaContent: "",
+    };
 
     handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({
@@ -12,31 +25,67 @@ export class SignIn extends Component {
         });
     }
 
-    handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        console.log('SignIn Request.', this.state);
+    handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        console.log("SignIn Request.", this.state);
 
-        fetch("https://localhost/user/5dc05905a712681a084c2d85")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                },
-                (error) => {
-                    console.log(error);
+        const singInModel: ISignInModel = {};
+        singInModel.username = this.state.username;
+        singInModel.password = this.state.password;
+
+        const loginRoute: string = `${API_ENDPOINT}/${AUTH_CONTROLLER_PATH}/${LOGIN_ACTION_PATH}`;
+        fetch(loginRoute, {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(singInModel),
+        })
+            .then(response => response.json())
+            .then(responseJson => {
+                console.log("responseJson", responseJson);
+
+                if (responseJson.token) {
+                    this.setState({ token: responseJson.token });
                 }
-            )
+            })
+            .catch(error => {
+                console.log("error", error);
+            })
+            .finally(() => {
+                alert("'signIn' Request completed.");
+            });
     }
 
-    render() {
+    handleClick = () => {
+        const getMeRoute: string = `${API_ENDPOINT}/${AUTH_CONTROLLER_PATH}/${LOGGED_IN_USER_INFORMATION_ACTION_PATH}`;
+        fetch(getMeRoute, {
+            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${this.state.token}` },
+        })
+            .then(response => response.json())
+            .then(response => {
+                this.setState({ textareaContent: JSON.stringify(response) });
+            })
+            .catch(error => {
+                console.log("error", error);
+            })
+            .finally(() => {
+                alert("'getMe' Request completed.");
+            });
+    }
+
+    render(): ReactElement {
         return (
-            <div className="signin-wrapper">
+            <div>
                 <p>SignIn - Form</p>
                 <form onSubmit={this.handleSubmit}>
                     <input type="username" id="username" onChange={this.handleChange} />
+                    <br />
                     <input type="password" id="password" onChange={this.handleChange} />
-                    <input type="submit" />
+                    <br />
+                    <input type="submit" value="Submit" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="button" value="My Info" onClick={this.handleClick} />
                 </form>
+                <textarea cols={50} rows={20} readOnly value={this.state.textareaContent} />
             </div>
         );
     }
