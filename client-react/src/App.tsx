c35d02c6d0d4952dafe2acdb690aa5bc69c13beb
Dmytro Morhun <dimamorgun97@gmail.com>
2019-11-06 13:38:40 +0200

    Object destructions has been added.

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import { SignIn } from "./components/auth/SignIn";
+import { Title } from "./components/layout/Title";
 
 class App extends Component {
-  render(): any {
+  render() {
     return (
-      <SignIn />
+      <Fragment>
+        <Title title="Simple SignIn App" />
+        <SignIn />
+      </Fragment>
     );
   }
 }
