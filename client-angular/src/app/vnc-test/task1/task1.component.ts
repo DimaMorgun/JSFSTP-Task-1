e25a87fe38a8de9227afa25856458d85c7cb51cb
Dmytro Morhun <dimamorgun97@gmail.com>
2019-11-22 14:38:55 +0200

    pollUntilTaskFinished method has been added.

--- client-angular/src/app/vnc-test/task1/task1.component.ts
+++ client-angular/src/app/vnc-test/task1/task1.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { timer } from 'rxjs'
+
 @Component({
-  selector: 'app-task1',
-  templateUrl: './task1.component.html',
-  styleUrls: ['./task1.component.scss']
+    selector: 'app-task1',
+    templateUrl: './task1.component.html',
+    styleUrls: ['./task1.component.scss']
 })
 export class Task1Component implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.pollUntilTaskFinished('51');
+    }
 
+    async pollUntilTaskFinished(taskId) {
+        let fetchResponse = {
+            processing: true,
+            status: '',
+        };
+
+        setTimeout(() => {
+            fetchResponse = {
+                processing: false,
+                status: 'IDI NAHUI',
+            };
+        }, 5000);
+
+        if (fetchResponse.processing) {
+            setTimeout(() => this.pollUntilTaskFinished(taskId), 500);
+        } else {
+            console.log('pollingFinishedFor', taskId);
+        }
+    }
 }
