88c009d41f31276dcdeddaa8881331c14b24da28
Dmytro Morhun <dimamorgun97@gmail.com>
2019-11-29 16:45:25 +0200

    Default code from tutorial 'https://makeitnew.io/polling-using-rxjs-8347d05e9104' has been added.

--- client-angular/src/app/vnc-test/task1/task1.component.ts
+++ client-angular/src/app/vnc-test/task1/task1.component.ts
@@ -1,74 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs';
-
 @Component({
     selector: 'app-task1',
     templateUrl: './task1.component.html',
     styleUrls: ['./task1.component.scss']
 })
-export class Task1Component implements OnInit {
 
+export class Task1Component implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        this.method1('123')
-            .switchMap(res => this.callMethod2EverySecond(res.id))
-            .subscribe(
-                res => {
-                    if (!res.success && res.errors) {
-                        console.error(res);
-                    } else {
-                        console.log('subscription processing', res);
-                    }
-                },
-                (err) => console.log(err),
-                () => console.log('finished')
-            );
-
-        this.pollUntilTaskFinished('51');
+        this.pollUntilTaskFinished('5');
     }
 
-    async pollUntilTaskFinished(taskId) {
-        let fetchResponse = {
-            processing: true,
-            status: '',
-        };
+    private async pollUntilTaskFinished(taskId: string): Promise<void> {
+        const fetchResponse = await fetch(`/tasks/${taskId}`);
+        const responseObject = await fetchResponse.json();
 
-        setTimeout(() => {
-            fetchResponse = {
-                processing: false,
-                status: 'Test',
-            };
-        }, 5000);
+        console.log(responseObject);
 
-        if (fetchResponse.processing) {
+        if (responseObject.processing) {
             setTimeout(() => this.pollUntilTaskFinished(taskId), 500);
         } else {
-            console.log('pollingFinishedFor', taskId);
+            this.pollingFinishedFor(taskId);
         }
     }
 
-    callMethod2EverySecond(id) {
-        return Observable.interval(10)
-            .mergeMap(data => this.method2(id, data))
-            .do(resp => console.log('resp', resp))
-            .filter(resp => resp !== null)
-            .take(1)
-    }
-
-    method1(data: string) {
-        return Observable.of({ id: data });
-    }
-
-    method2(id: string, interval: number) {
-        const ret = this.randomIntInc(0, 1) === 0 ? null : { success: true, errors: null, interval, id };
-        return Observable
-            .of(ret)
-            .delay(this.randomIntInc(0, 2000));
-    }
-
-    randomIntInc(low, high) {
-        return Math.floor(Math.random() * (high - low + 1) + low);
+    private pollingFinishedFor(taskId: string): void {
+        console.log(`Task with id ${taskId} has been finished.`);
     }
 }
